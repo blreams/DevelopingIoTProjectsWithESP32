789d964b78da2b0cedef229fce8c82f5e49c42c7
Byron Reams <blreams@gmail.com>
2025-06-08 23:52:15 -0400

    Taskified the project

--- i2c_esp32_1/main/main.cpp
+++ i2c_esp32_1/main/main.cpp
@@ -7,63 +7,47 @@
 #include "driver/i2c_master.h"
 #include "esp_log.h"
 #include "esp_idf_version.h"
+#include "sdkconfig.h"
 
 #include "AppI2cMaster.hpp"
 #include "AppGpioOut.hpp"
 #include "AppBme280.hpp"
 
-static const char *TAG = "i2c-master";
+extern "C" {
 
-extern "C" void app_main()
-{
-    printf("ESP-IDF version: %s\n", esp_get_idf_version());
-
-    AppGpioOut led(GPIO_NUM_2);
-    AppI2cMaster i2c;
-    const std::string bme_name = "bme280";
-    AppBme280 bme280(bme_name);
-
-
-    // initialize objects
-    ESP_ERROR_CHECK(led.init());
-    ESP_ERROR_CHECK(i2c.init());
-
-    bme280_config_t bme_config = {
-        .address = 0x77,
-        .freq = I2C_MAX_FREQ_HZ,
-        .read_latency = 0,
-        .i2c = &i2c
+    void vBlinkTask(void* pvParameters) {
+        AppGpioOut led(GPIO_NUM_2);
+        ESP_ERROR_CHECK(led.init());
+        while (true) {
+            led.set(!led.get());
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
     };
-    ESP_ERROR_CHECK(bme280.init(bme_config));
 
+    void vGetTempTask(void* pvParameters) {
+        AppI2cMaster i2c;
+        const std::string bme_name = "bme280";
+        AppBme280 bme280(bme_name);
+        ESP_ERROR_CHECK(i2c.init());
+        bme280_config_t bme_config = {
+            .address = 0x77,
+            .freq = I2C_MAX_FREQ_HZ,
+            .read_latency = 0,
+            .i2c = &i2c
+        };
+        ESP_ERROR_CHECK(bme280.init(bme_config));
 
-    //i2c.scan_bus();
+        vTaskSuspend(nullptr);
+    };
 
-    /*
-    ESP_ERROR_CHECK(i2c.add_device("bme280", 0x77, I2C_MAX_FREQ_HZ));
-    DeviceInfo dev = i2c.get_device_by_name("bme280");
+    void app_main() {
+        printf("ESP-IDF version: %s\n", esp_get_idf_version());
 
-    uint8_t reg = 0xd0;
-    uint8_t read_buffer = 0;
+        // create a task to blink the LED
+        xTaskCreate(vBlinkTask, "blink_task", 4096, nullptr, 5, nullptr);
+        xTaskCreate(vGetTempTask, "get_temp_task", 4096, nullptr, 5, nullptr);
 
-    led.set(true);
-    ESP_ERROR_CHECK(i2c.read_bytes("bme280", reg, 1, &read_buffer));
-    led.set(false);
-    ESP_LOGD(TAG, "Register %x read_buffer value is %x", 0xd0, read_buffer);
-    */
+        vTaskSuspend(nullptr);
 
-    vTaskSuspend(NULL);
-
-    /*
-    // 3. Transmit some data
-    uint8_t bytes[] = {0x00, 0xA5};  // Example: write two bytes
-    ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, bytes, sizeof(bytes), -1));
-    ESP_LOGI(TAG, "I2C write complete");
-
-    // 4. Optionally receive
-    uint8_t rx = 0;
-    uint8_t reg = 0x01;
-    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, &reg, 1, &rx, 1, -1));
-    ESP_LOGI(TAG, "I2C read: 0x%02X", rx);
-    */
-}
+    };
+};
